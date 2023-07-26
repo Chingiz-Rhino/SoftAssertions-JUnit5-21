b04e703531644517f6d3468024ce063cf84cdb99
Chingiz <chingiz-askarov@list.ru>
2023-07-26 21:21:55 +0300

    Find SoftAssertions in Pages and click SoftAssertions

--- src/test/java/guru/qa/SelenideWikiTest.java
+++ src/test/java/guru/qa/SelenideWikiTest.java
@@ -19,12 +19,13 @@ public class SelenideWikiTest {
         //Переходим на страницу Wiki
         $("#wiki-tab").click();
 
-        //Проверяем что на странице в раделе Pages, есть страница с названием "Soft assertions"
-        $(".f6.Link--muted.js-wiki-more-pages-link.btn-link.mx-auto").click();
-        $("a[href='/selenide/selenide/wiki/SoftAssertions']").shouldHave(text("SoftAssertions"));
+        //Кликаем на кнопку Show more в колонке Pages и выбираем SoftAssertions
+        $("#wiki-pages-box button").click();
+        $("#wiki-pages-box").shouldHave(text("SoftAssertions"));
 
-        //Переходим на страницу "Soft assertions"
-        $(byText("Soft assertions")).click();
+        //Переходим в SoftAssertions
+        $("[href='/selenide/selenide/wiki/SoftAssertions']").click();
+
 
         //Проверяем, что на странице есть пример кода для JUnit5
         $(".markdown-body").$(byText("3. Using JUnit5 extend test class:")).sibling(0)
